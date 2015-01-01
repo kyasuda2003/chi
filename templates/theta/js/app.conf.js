644f30bb760cc5bda4c3445aea6836fc7892c3ad
kyasuda2003 <kyasuda2003@gmail.com>
2015-01-01 09:44:36 -0800

    Get APIs ready.

--- templates/theta/js/app.conf.js
+++ templates/theta/js/app.conf.js
@@ -1,18 +1,95 @@
 (function() {
   var app;
+  
+  //override
+  String.prototype.format=function() {
+        var args = Array.prototype.slice.call(arguments, 0);
+        return this.toString().replace(/{(\d+)}/g, function(match, number) {
+          return typeof args[number] != 'undefined'
+            ? args[number]
+            : match
+          ;
+        });
+    };
 
   app = {
     settings: {
+      dev: true,  
       stage: ["main", "product", "company", "news", "people"][0],
-      path: ["/assets/"][0]
+      apipath: ["/"][0],
+      apihost: ["http://localhost:9000"][0],
+      apppath: ["/theta/","/assets/"][1]
+    },
+    util:{
+        array2json:function(arr,jsn,key){
+            for (var obj in arr)
+                jsn[arr[obj][key].toString()]=arr[obj];
+        }
+    },
+    ui:{
+        popupBlock: function(blockId, colour, opac, display) {
+            $('#' + blockId).each(function(index, val) {
+                $(this).remove();
+            });
+            return $('<div id=\'' + blockId + '\' class=\'block-window\'></div>').css({
+                'height': '100%',
+                'width': '100%',
+                'z-index': '4000',
+                'opacity': opac,
+                'position': 'fixed',
+                'top': '0',
+                'left': '0',
+                'display': display ? 'block' : 'none',
+                'background-color': colour,
+                'background-image': colour === 'white' ? 'url(\''+app.settings.apppath+'img/temp-load.gif\')' : '',
+                'background-repeat': 'no-repeat',
+                'background-position':'center'});
+        },
+        switchView: function(win,path){
+            if (win.location.href.indexOf(path)>-1)
+                return;
+            
+            $('body').append(app.ui.popupBlock('login-block', 'white', '1', false));
+            $('#login-block').fadeIn(1000, function(){
+                win.location.replace(win.location.pathname+path);
+            });
+        }
     },
     api: (function() {
+      var _this=this;
+      
       return {
+        getAllSizes: function() {
+          return $.get("{0}{1}obj/sizes/".format(app.settings.apihost,app.settings.apipath)).success(function(data, textStatus, jqxhr){
+              app.util.array2json(data.results,app.data.sizes,'id');
+          });
+        },
         getAllCategories: function() {
-          return $.get('/');
+          return $.get("{0}{1}obj/categories/".format(app.settings.apihost,app.settings.apipath)).success(function(data, textStatus, jqxhr){
+                app.util.array2json(data.results,app.data.categories,'id');
+          
+          });
+        },
+        getAllPhotos: function() {
+          return $.get("{0}{1}obj/photos/".format(app.settings.apihost,app.settings.apipath)).success(function(data, textStatus, jqxhr){
+                  app.util.array2json(data.results,app.data.photos,'id');
+          
+          });
+        },
+        getAllProducts: function() {
+          return $.get("{0}{1}obj/products/".format(app.settings.apihost,app.settings.apipath)).success(function(data, textStatus, jqxhr){
+                  app.util.array2json(data.results,app.data.products,'id');
+          
+          });
         }
       };
-    })()
+    })(),
+    data: {
+      sizes: {},
+      categories: {},
+      photos: {},
+      products: {}
+    }
   };
 
   window.app = app;
--- templates/theta/js/app.js
+++ templates/theta/js/app.js
@@ -1,8 +1,10 @@
 'use strict';
+
 // Declare app level module which depends on filters, and services
 angular.module('poeApp', ['poeApp.filters', 'poeApp.services', 'poeApp.directives', 'poeApp.controllers']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/main', {templateUrl: './assets/partials/alpha.html', controller: 'alpha'});
-    $routeProvider.when('/product', {templateUrl: './assets/partials/beta.html', controller: 'beta'});
+     
+    $routeProvider.when('/main', {templateUrl: app.settings.apppath+'partials/alpha.html', controller: 'alpha'});
+    $routeProvider.when('/product', {templateUrl: app.settings.apppath+'partials/beta.html', controller: 'beta'});
     $routeProvider.otherwise({redirectTo: '/main'});
   }]);
