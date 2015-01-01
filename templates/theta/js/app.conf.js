7d423d964e1c48e5386b0cf63bb120177ee187c0
kyasuda2003 <kyasuda2003@gmail.com>
2015-01-01 13:46:49 -0800

    Get APIs ready. V

--- templates/theta/js/app.conf.js
+++ templates/theta/js/app.conf.js
@@ -1,16 +1,12 @@
 (function() {
-  var app;
-
-  app = {
-    settings: {
-      dev: true,
-      stage: ["main", "product", "company", "news", "people"][0],
-      apipath: ["/"][0],
-      apihost: ["http://localhost:9000", ""][1],
-      apppath: ["/theta/", "/assets/"][1]
-    }
+ var app={};
+  app.settings = {
+    dev: true,
+    stage: ["main", "product", "company", "news", "people"][0],
+    apipath: ["/"][0],
+    apihost: ["http://localhost:9000", ""][1],
+    apppath: ["/theta/", "/assets/"][1]
   };
-
-  window.app = app;
+  window.app=app;
 
 }).call(this);
--- templates/theta/lib/poe/poe-lib.js
+++ templates/theta/lib/poe/poe-lib.js
@@ -1,8 +1,7 @@
 (function() {
-  var app;
-
   String.prototype.format = function() {
     var args;
+    args = void 0;
     args = Array.prototype.slice.call(arguments, 0);
     return this.toString().replace(/{(\d+)}/g, function(match, number) {
       if (typeof args[number] !== "undefined") {
@@ -12,96 +11,98 @@
       }
     });
   };
-
-  app = {
-    util: {
-      array2json: function(arr, jsn, key) {
-        var obj;
-        for (obj in arr) {
-          jsn[arr[obj][key].toString()] = arr[obj];
-        }
+  
+  app.util = {
+    array2json: function(arr, jsn, key) {
+      var obj;
+      obj = void 0;
+      for (obj in arr) {
+        jsn[arr[obj][key].toString()] = arr[obj];
       }
-    },
-    ui: {
-      popupBlock: function(blockId, colour, opac, display) {
-        $("#" + blockId).each(function(index, val) {
-          $(this).remove();
-        });
-        return $("<div id='" + blockId + "' class='block-window'></div>").css({
-          height: "100%",
-          width: "100%",
-          "z-index": "4000",
-          opacity: opac,
-          position: "fixed",
-          top: "0",
-          left: "0",
-          display: (display ? "block" : "none"),
-          "background-color": colour,
-          "background-image": (colour === "white" ? "url('" + app.settings.apppath + "img/temp-load.gif')" : ""),
-          "background-repeat": "no-repeat",
-          "background-position": "center"
-        });
-      },
-      switchView: function(win, path) {
-        if (win.location.href.indexOf(path) > -1) {
-          return;
-        }
-        $("body").append(app.ui.popupBlock("login-block", "white", "1", false));
-        $("#login-block").fadeIn(1000, function() {
-          win.location.replace(win.location.pathname + path);
-        });
-      }
-    },
-    api: (function() {
-      var _this;
-      _this = this;
-      return {
-        getAllSizes: function() {
-          if ($.isEmptyObject(app.data.sizes)) {
-            return $.Deferred().resolve();
-          } else {
-            return $.get("{0}{1}obj/sizes/".format(app.settings.apihost, app.settings.apipath)).success(function(data, textStatus, jqxhr) {
-              app.util.array2json(data.results, app.data.sizes, "id");
-            });
-          }
-        },
-        getAllCategories: function() {
-          if ($.isEmptyObject(app.data.categories)) {
-            return $.Deferred().resolve();
-          } else {
-            return $.get("{0}{1}obj/categories/".format(app.settings.apihost, app.settings.apipath)).success(function(data, textStatus, jqxhr) {
-              app.util.array2json(data.results, app.data.categories, "id");
-            });
-          }
-        },
-        getAllPhotos: function() {
-          if ($.isEmptyObject(app.data.photos)) {
-            return $.Deferred().resolve();
-          } else {
-            return $.get("{0}{1}obj/photos/".format(app.settings.apihost, app.settings.apipath)).success(function(data, textStatus, jqxhr) {
-              app.util.array2json(data.results, app.data.photos, "id");
-            });
-          }
-        },
-        getAllProducts: function() {
-          if ($.isEmptyObject(app.data.products)) {
-            return $.Deferred().resolve();
-          } else {
-            return $.get("{0}{1}obj/products/".format(app.settings.apihost, app.settings.apipath)).success(function(data, textStatus, jqxhr) {
-              app.util.array2json(data.results, app.data.products, "id");
-            });
-          }
-        }
-      };
-    })(),
-    data: {
-      sizes: {},
-      categories: {},
-      photos: {},
-      products: {}
     }
   };
 
-  window.app = app;
+  app.ui = {
+    popupBlock: function(blockId, colour, opac, display) {
+      $("#" + blockId).each(function(index, val) {
+        $(this).remove();
+      });
+      return $("<div id='" + blockId + "' class='block-window'></div>").css({
+        height: "100%",
+        width: "100%",
+        "z-index": "4000",
+        opacity: opac,
+        position: "fixed",
+        top: "0",
+        left: "0",
+        display: (display ? "block" : "none"),
+        "background-color": colour,
+        "background-image": (colour === "white" ? "url('" + app.settings.apppath + "img/temp-load.gif')" : ""),
+        "background-repeat": "no-repeat",
+        "background-position": "center"
+      });
+    },
+    switchView: function(win, path) {
+      if (win.location.href.indexOf(path) > -1) {
+        return;
+      }
+      $("body").append(app.ui.popupBlock("login-block", "white", "1", false));
+      $("#login-block").fadeIn(1000, function() {
+        win.location.replace(win.location.pathname + path);
+      });
+    }
+  };
+
+  app.api = (function() {
+    var _this;
+    _this = void 0;
+    _this = this;
+    return {
+      getAllSizes: function() {
+        if ($.isEmptyObject(app.data.sizes)) {
+          return $.Deferred().resolve();
+        } else {
+          return $.get("{0}{1}obj/sizes/".format(app.settings.apihost, app.settings.apipath)).success(function(data, textStatus, jqxhr) {
+            app.util.array2json(data.results, app.data.sizes, "id");
+          });
+        }
+      },
+      getAllCategories: function() {
+        if ($.isEmptyObject(app.data.categories)) {
+          return $.Deferred().resolve();
+        } else {
+          return $.get("{0}{1}obj/categories/".format(app.settings.apihost, app.settings.apipath)).success(function(data, textStatus, jqxhr) {
+            app.util.array2json(data.results, app.data.categories, "id");
+          });
+        }
+      },
+      getAllPhotos: function() {
+        if ($.isEmptyObject(app.data.photos)) {
+          return $.Deferred().resolve();
+        } else {
+          return $.get("{0}{1}obj/photos/".format(app.settings.apihost, app.settings.apipath)).success(function(data, textStatus, jqxhr) {
+            app.util.array2json(data.results, app.data.photos, "id");
+          });
+        }
+      },
+      getAllProducts: function() {
+        if ($.isEmptyObject(app.data.products)) {
+          return $.Deferred().resolve();
+        } else {
+          return $.get("{0}{1}obj/products/".format(app.settings.apihost, app.settings.apipath)).success(function(data, textStatus, jqxhr) {
+            app.util.array2json(data.results, app.data.products, "id");
+          });
+        }
+      }
+    };
+  })();
+
+  app.data = {
+    sizes: {},
+    categories: {},
+    photos: {},
+    products: {}
+  };
+  
 
 }).call(this);
